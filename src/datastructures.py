87eb689fc73276d090ddfd1d528f7a617a53eecf
Marii2806 <107720808+Marii2806@users.noreply.github.com>
2024-03-13 21:31:44 +0000

    All test have passed

--- src/datastructures.py
+++ src/datastructures.py
@@ -25,24 +25,24 @@ class FamilyStructure:
 # Lucky Numbers: 1
         # example list of members
         self._members = [{
-            # "id": self._generateId(),
-            "id": 3441,
+            "id": self._generateId(),
+            # "id": 3441,
             "first_name": "John",
             "last_name": last_name,
             "age": 33,
             "lucky_numbers": [7, 13, 22]
         },
         {
-            # "id": self._generateId(),
-            "id": 3442,
+            "id": self._generateId(),
+            # "id": 3442,
             "first_name": "Jane",
             "last_name": last_name,
             "age": 35,
             "lucky_numbers": [10, 14, 3]
         },
         {
-            # "id": self._generateId(),
-            "id": 3443,
+            "id": self._generateId(),
+            # "id": 3443,
             "first_name": "Jimmy",
             "last_name": last_name,
             "age": 5,
@@ -56,24 +56,22 @@ class FamilyStructure:
         return randint(0, 99999999)
 
     def add_member(self, member):
-        new_member = {
-            "id": self._generateId(),
-            "first_name": member,
-            "last_name": self.last_name,
-            # "age": member[ "age"],
-            # "lucky_numbers": member["lucky_numbers"]
-        }
-        self._members.append(new_member)
-        return self._members
-        # return new_member
+        if id not in member:
+            member["id"]=self._generateId()
+        member["last_name"]=self.last_name
+        if member["age"] and member["first_name"] and member ["lucky_numbers"]:
+            self._members.append(member)
+            return self._members
+        else: 
+            return False
 
     def delete_member(self, id):
         # fill this method and update the return
         for member in self._members:
             if id == member["id"]:
                 self._members.remove(member)
-                return self._members
-        return None
+                return True
+            return False
 
     def get_member(self, id):
         # fill this method and update the return
